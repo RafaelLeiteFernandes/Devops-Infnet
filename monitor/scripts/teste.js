2bd3601c376247f4b7754482b6985edeb2f1757a
rafaelleitefernandes <rafael.fernandesAimec@gmail.com>
2025-04-08 23:40:31 -0300

    save

--- monitor/scripts/teste.js
+++ monitor/scripts/teste.js
@@ -1,15 +1,13 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { sleep, check } from 'k6';
 
-export const options = {
-  vus: 10,
+export let options = {
+  vus: 100,
   duration: '30s',
 };
 
 export default function () {
-  const res = http.get('http://host.docker.internal:3000');
-  check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
+  let res = http.get("http://guia-infnet");
+  check(res, { 'status is 200': (r) => r.status === 200 });
   sleep(1);
 }
